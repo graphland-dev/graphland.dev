457e0d7136a94f1bd354e0f29d57c088b88c32fe
kingrayhan <rayhan095@gmail.com>
2026-05-31 19:06:36 +0600

    chore: update Next.js configuration to include asset prefix for production
    - Added assetPrefix to next.config.ts for serving assets from a CDN in production environment.

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   // Ensure Next.js traces files relative to this project, not a parent folder
   outputFileTracingRoot: process.cwd(),
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "https://graphland.b-cdn.net" : "",
   images: { remotePatterns: [new URL("https://cdn.graphland.dev/**")] },
 };
 
