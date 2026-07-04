3abbf3a14933d11870b0ef59ec675a36b90453fa
kingrayhan <rayhan095@gmail.com>
2026-07-04 11:59:13 +0600

    chore: simplify Next.js configuration by removing assetPrefix
    - Removed assetPrefix from next.config.ts to streamline configuration as it is no longer needed for production.

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  assetPrefix:
-    process.env.NODE_ENV === "production" ? "https://graphland.b-cdn.net" : "",
-  /* config options here */
-  // Ensure Next.js traces files relative to this project, not a parent folder
   outputFileTracingRoot: process.cwd(),
   images: {
     remotePatterns: [
